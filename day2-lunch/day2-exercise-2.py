4fb98e4ad26857aba860a94df3d8b0a291a99fc4
Jeremiah Miller <jmill3082@jhu.edu>
2016-08-30 14:06:06 -0400

    Second and Third Exercises Complete

---
+++ day2-lunch/day2-exercise-2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+f = open( sys.argv[1] )
+
+count = 0
+
+for line in f.readlines():
+    if "NM:i:0" in line:
+        count += 1
+    else:
+        continue
+
+print count
+   
+f.close()
---
+++ day2-lunch/day2-exercise-3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+f = open( sys.argv[1] )
+
+count = 0
+
+for line in f.readlines():
+    if "NH:i:1" in line:
+        count += 1
+    else:
+        continue
+
+print count
+   
+f.close()

4f336f3ffa881f1a6363915248074d3ac2bf8460
Jeremiah Miller <jmill3082@jhu.edu>
2016-09-01 18:41:39 -0400

    Second Exercise Complete

---
+++ day4-homework/02-histogram.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+Makes a histogram of log(FPKM) values from a ctab file.
+"""
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_table( sys.argv[1] )
+df_roi = df[ "FPKM" ] > 0
+df_hist = df[ df_roi ][ "FPKM" ]
+
+# Diagnostic
+# print type(df_roi)
+# print df_roi
+# print type(df_hist)
+# print df_hist
+
+hist_values = np.log10(df_hist)
+
+# Diagnostic
+# print hist_values
+
+# Determining the min and max values in the array for binning purposes
+# print min(hist_values)
+# print max(hist_values)
+
+plt.figure()
+n, bins, patches = plt.hist(hist_values, range = [-4, 4], bins = 50)
+plt.savefig( "FPKM-hist.png" )
+plt.title( "Frequency of log(FPKM)" )
+plt.xlabel( "log(FPKM)" )
+plt.ylabel( "occurrences of log(FPKM)" )
+plt.show()
